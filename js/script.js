c1a9ca746f23f80c4b0550455c1e99c911123a77
will <test@mail.com>
2020-08-10 18:57:09 +0300

    work with events

--- js/script.js
+++ js/script.js
@@ -1,61 +1,93 @@
 'use strict';
-/* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+document.addEventListener('DOMContentLoaded', () => {
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll(".promo__adv img");
+    const poster = document.querySelector(".promo__bg");
+    const genre = poster.querySelector(".promo__genre"); 
+    const movieList = document.querySelector(".promo__interactive-list");
+    const listItem = movieList.querySelector(".promo__interactive-item");
+    const addForm = document.querySelector("form.add");  
+    const addInput = addForm.querySelector(".adding__input");
+    const checkbox = addForm.querySelector('[type="checkbox"]');
+    
+    const deleteAdv = (arr) => {
+        arr.forEach((item) => {
+            item.remove();
+        });
+    };
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+    const makeChanges = () => {  
+        genre.textContent = "Драма";
+        poster.style.background = "url('../img/bg.jpg') center/cover no-repeat";
+    };
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+    const sortArr = (arr) => {
+        arr.sort();
+    }; 
 
-5) Добавить нумерацию выведенных фильмов */
+    function createMovieList(films, parent) {
+        sortArr(films);
+        parent.innerHTML = "";
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${++i}. ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
 
-// 1. получаем рекламный элемент
+        document.querySelectorAll(".delete").forEach( (btn, i) => {
+            btn.addEventListener("click", () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
 
-const adv = document.querySelectorAll(".promo__adv img");
+                createMovieList(films, parent);
+            });
+        });
+    }  
+    
+    addForm.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-adv.forEach((item) => {
-    item.remove();
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            if(favorite) {
+                console.log("Вы выбрали любимый фильм");
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+
+            // нужно снова перебрать базу данных, т.к. там новое значение
+            createMovieList(movieDB.movies, movieList);
+
+        }
+
+        //очистка формы на элементе, на котором происходит событие
+        event.target.reset();
+    });
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
 });
-
-// 2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-const poster = document.querySelector(".promo__bg"),
-      genre = poster.querySelector(".promo__genre");
-
-genre.textContent = "Драма";
-
-// 3) Изменить задний фон постера с фильмом на изображение "bg.jpg"
-
-poster.style.background = "url('../img/bg.jpg') center/cover no-repeat";
-
-//4) Список фильмов на странице сформировать на основании данных из этого JS файла.Отсортировать их по алфавиту 
-
-
-const movieList = document.querySelector(".promo__interactive-list");
-movieList.innerHTML = ""; //очищаем
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${++i}. ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
-
 
